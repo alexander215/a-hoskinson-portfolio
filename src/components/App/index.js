833d27b55b5ffe2ac9e83283271fb17fb9d79333
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-13 21:11:27 -0700

    Create component file structure

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import IntroHeader from '../IntroHeader';
+
+function App() {
+  return (
+    <div className="App">
+      This is my site.
+      <IntroHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/IntroHeader/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IntroHeader = () => {
+    return (
+        <div>
+            This is my intro header.
+        </div>
+    )
+
+}
+
+export default IntroHeader;
