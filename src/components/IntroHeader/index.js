e415dba381162cb646a1009894ca0702198cba17
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-14 18:06:29 -0700

    Basic header colors and layout

--- src/components/IntroHeader/index.js
+++ src/components/IntroHeader/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const IntroHeader = () => {
     return (
-        <div class="intro-header">
-            <h1 class="intro-title">Alexander Hoskinson</h1>
-            <h3>Full-Stack Web Developer</h3>
+        <div class="main-header">
+            <div class="intro-header">
+                <div class="intro-text">
+                    <h1 class="intro-title">Alexander Hoskinson</h1>
+                    <h3 class="intro-sub">Full-Stack Web Developer</h3>
+                </div>
+            </div>
         </div>
     )
 
