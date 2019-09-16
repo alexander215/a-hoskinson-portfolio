791860ee28ac9256a2f92374a8289af85de611f3
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-15 23:34:33 -0700

    Add noopener to a href in Contact and ComingSoon

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,9 +4,9 @@ const Contact = () => {
     return (
         <div>
             <h2>Let's Connect!</h2>
-            <a href="https://www.linkedin.com/in/alexhoskinson/" target="_blank" rel="noreferrer">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/alexhoskinson/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
              | 
-            <a href="https://github.com/alexander215" target="_blank" rel="noreferrer">GitHub</a>
+            <a href="https://github.com/alexander215" target="_blank" rel="noopener noreferrer">GitHub</a>
         </div>
     )
 }
