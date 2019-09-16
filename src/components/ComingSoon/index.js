abca9672c539c0df1c49de0b3d4a430f4558cc61
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-15 23:54:40 -0700

    Full https name in package.json

--- src/components/ComingSoon/index.js
+++ src/components/ComingSoon/index.js
@@ -4,7 +4,7 @@ const ComingSoon = () => {
     return (
         <div>
             Projects and additional info coming soon. 
-            Until then, let's connect <a href="https://www.linkedin.com/in/alexhoskinson/" target="_blank" rel="noreferrer">LinkedIn</a> and <a href="https://github.com/alexander215" target="_blank" rel="noreferrer">GitHub</a>.
+            Until then, let's connect <a href="https://www.linkedin.com/in/alexhoskinson/" target="_blank" rel="noopener noreferrer">LinkedIn</a> and <a href="https://github.com/alexander215" target="_blank" rel="noopener noreferrer">GitHub</a>.
         </div>
     )
 }
