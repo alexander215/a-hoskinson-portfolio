b8b199182982506dacd08b1cbf0d3fe7c9047cb0
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-20 00:16:49 -0700

    Complete styled component transer for intro header

--- src/components/IntroHeader/index.js
+++ src/components/IntroHeader/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { IntroHeaderContainer } from './style.js';
+import { IntroHeaderContainer, IntroTextContainer, IntroTitle, IntroSub } from './style.js';
 
 const IntroHeader = () => {
     return (
         <IntroHeaderContainer>
-            <div class="intro-header">
-                <div class="intro-text">
-                    <h1 class="intro-title">Alexander Hoskinson</h1>
-                    <h3 class="intro-sub">Full-Stack Web Developer</h3>
-                </div>
-            </div>
+                <IntroTextContainer>
+                    <IntroTitle>
+                        Alexander Hoskinson 
+                    </IntroTitle>
+                    <IntroSub>
+                        Full-Stack Web Developer
+                    </IntroSub>
+                </IntroTextContainer>
+                
         </IntroHeaderContainer>
     )
 
--- src/components/IntroHeader/style.js
+++ src/components/IntroHeader/style.js
@@ -9,26 +9,21 @@ export const IntroHeaderContainer = styled.div`
     clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
     
 `
+export const IntroTextContainer = styled.div`
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
 
-// .main-header {
-//     background-color: black;
-//   }
-//   .intro-header {
-//   }
-  
-//   .intro-text {
-//     position: absolute;
-//     top: 40%;
-//     left: 50%;
-//     transform: translate(-50%, -50%);
-//     text-align: center;
-//   }
-//   .intro-sub {
-//     /* margin: 100 0 0 0; */
-//     margin: 0;
-//     color: black;
-//   }
-//   .intro-title {
-//     padding: .5rem 0 0 0;
-//     margin: 0;
-//   }
+`
+export const IntroTitle = styled.h1`
+    padding: .5rem 0 0 0;
+    margin: 0;
+`
+
+export const IntroSub = styled.h3`
+    margin: 0;
+    color: black;
+`
+// margin: 100 0 0 0;
