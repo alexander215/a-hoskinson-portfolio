8017040764133dad592baca637360e2cbc11e5b7
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-20 18:56:21 -0700

    Responsive intro section

--- src/components/ComingSoon/index.js
+++ src/components/ComingSoon/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ComingSoonContainer } from './style';
 
 const ComingSoon = () => {
     return (
-        <div>
+        <ComingSoonContainer>
             Projects and additional info coming soon. 
             Until then, let's connect <a href="https://www.linkedin.com/in/alexhoskinson/" target="_blank" rel="noopener noreferrer">LinkedIn</a> and <a href="https://github.com/alexander215" target="_blank" rel="noopener noreferrer">GitHub</a>.
-        </div>
+        </ComingSoonContainer>
+        
     )
 }
 
---
+++ src/components/ComingSoon/style.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import { deviceSize } from '../App/device.js'
+
+export const ComingSoonContainer = styled.div`
+    padding: normal;
+    margin: 2.3rem;
+    @media ${deviceSize.mobileS}{
+
+    }
+
+    @media ${deviceSize.mobileM} {
+        
+    }
+
+    @media ${deviceSize.mobileL}{
+        
+    }
+    @media ${deviceSize.tablet}{
+
+    }
+    @media ${deviceSize.laptop}{
+
+    }
+    @media ${deviceSize.laptopL}{
+
+    }
+    @media ${deviceSize.desktop}{
+
+    }
+
+`
