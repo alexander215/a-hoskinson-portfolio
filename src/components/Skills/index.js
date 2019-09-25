393a0831a8d89cff22d678eeaffcfde8febe1d50
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-24 23:47:58 -0600

    Basic layout of skills list

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,53 +1,64 @@
 import React from 'react';
+import { SectionHeaderYellow } from '../App/style';
+import { SkillsContainer, SkillsTextContainer, SkillsList, SkillsListItem } from './style';
+
 
 const Skills = () => {
     return (
-        <div>
-        <h2>
-            Technical Skills:
-        </h2>
-            <ul>
-                <li>
-                    HTML5
-                </li>
-                <li>
-                    CSS3
-                </li>
-                <li>
-                    JavaScript ES6
-                </li>
-                <li>
-                    Git
-                </li>
-                <li>
-                    GitHub
-                </li>
-                <li>
-                    NodeJS
-                </li>
-                <li>
-                    ReactJS
-                </li>
-                <li>
-                    Express
-                </li>
-                <li>
-                    Python
-                </li>
-                <li>
-                    MongoDB
-                </li>
-                <li>
-                    PostgreSQL
-                </li>
-                <li>
-                    Flask
-                </li>
-                <li>
-                    Sass
-                </li>
-            </ul>
-        </div>
+        <SkillsContainer>
+            <SkillsTextContainer>
+                <SectionHeaderYellow>
+                    Technical Skills:
+
+                </SectionHeaderYellow>
+                <SkillsList>
+                    <SkillsListItem>
+                        HTML5
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        CSS3
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        JavaScript ES6
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        Git
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        GitHub
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        NodeJS
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        ReactJS
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        Express
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        Python
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        MongoDB                
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        PostgreSQL                                
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        Flask                                
+                    </SkillsListItem>
+                    <SkillsListItem>
+                        Sass
+                    </SkillsListItem>
+                </SkillsList>
+            
+                
+                
+
+            </SkillsTextContainer>
+        </SkillsContainer>
+        
     )
 }
  export default Skills;
---
+++ src/components/Skills/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const SkillsContainer = styled.div`
+    margin: 0;
+    `
+export const SkillsTextContainer = styled.div`
+    max-width: 70%;
+    margin: 5% auto 5% auto;
+    `
+export const SkillsList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    color: white;
+    max-width: 80%;
+    margin: 0 auto;
+    padding: 5% 0 0 0;
+    text-align: center;
+`
+
+export const SkillsListItem = styled.div`
+    padding: 3% 3%;
+
+`
