6c1c6d81d2511800b74ea25e082eddc5647ae542
alexander215 <alexander.hoskinson@colorado.edu>
2019-09-28 23:01:10 -0600

    Skills list centered, flex-grow, and h3

--- src/components/Skills/style.js
+++ src/components/Skills/style.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { deviceSize } from '../App/device.js'
+
 
 export const SkillsContainer = styled.div`
     margin: 0;
@@ -15,9 +17,9 @@ export const SkillsList = styled.div`
     margin: 0 auto;
     padding: 5% 0 0 0;
     text-align: center;
-`
-
-export const SkillsListItem = styled.div`
+    `
+    
+    export const SkillsListItem = styled.h3`
     padding: 3% 3%;
-
+    flex-grow: 1;
 `
