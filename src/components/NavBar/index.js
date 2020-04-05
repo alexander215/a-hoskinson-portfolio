cd6fc1c20583e9e0cc0c5afc20cfcb939089c9fd
alexander215 <alexander.hoskinson@colorado.edu>
2020-04-05 15:46:40 -0700

    Create NavBar component and populate with content from abandoned NavBar v1

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { Link, Switch } from 'react-router-dom';
+// import * as ROUTES from '../../constants/routes';
+// import { NavBarSection } from '../App/style';
+// import Link from 'react-router-dom';
+
+const NavBar = () => {
+    return(
+        <div>
+            <a href='#about'>About</a>
+            <a href='#projects'>Projects</a>
+            <a href='#skills'>Skills</a>
+            <a href='#experience'>Experience</a>
+            <a href='#education'>Education</a>
+            <a href='#about'>About</a>
+        </div>
+    )
+
+
+}
+
+export default NavBar;
