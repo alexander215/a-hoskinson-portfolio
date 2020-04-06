ce16d131ef7aba036ed68a6791098582dccff105
alexander215 <alexander.hoskinson@colorado.edu>
2020-04-05 22:45:02 -0700

    Responsive spacing for nav bar

--- src/components/NavBar/style.js
+++ src/components/NavBar/style.js
@@ -5,19 +5,25 @@ import { deviceSize } from '../App/device.js'
 export const NavBarStyle = styled.div`
   margin: 0 auto;
   padding: 1em;
-  max-width: 60%;
+  max-width: 80%;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
   a {
     color: white;
     text-decoration: none;
+    padding: 0.1em 1em;
+    @media ${deviceSize.mobileL}{
+      padding: 0;
+    }
   }
   a:hover {
     text-transform: uppercase;
     border-bottom: 2px solid #F9DC24;
   }:
   @media ${deviceSize.mobileL}{
-    background-color: orange;
-    max-width: 10%
+    background-color: black;
+    justify-content: space-between;
+    max-width: 60%;
   }
 `
