bbb38bf123336985821c5499bf5cb1f8948e05d0
alexander215 <alexander.hoskinson@colorado.edu>
2021-07-14 22:24:17 -0700

    Remove old code

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,13 +17,6 @@ const About = () => {
                     <br/>
                     <br/>
                     Prior to my career pivot into the engineering world in 2019, I spent years working in various marketing roles where I was heavily focused on UX/UI Design and was often tasked with managing &amp; maintaining digital properties.
-                    {/* Hi, <b>I’m Alex</b> - a full-stack developer with a passion for creating enjoyable React apps. 
-                    <br/>
-                    <br/>
-                    I have the heart of an explorer, and I spent much of the past decade living out of my comfort zone in different corners of Europe. I learned to love the challenges of life abroad, and I perfected the art of adapting to new situations. 
-                    <br/>
-                    <br/>
-                    That’s also what I love about programming. An impossible task is easy when broken down into a million simple steps, and the struggle is what makes the result so enjoyable. */}
                 </AboutHeadingText>
             </AboutTextContainer>
         </AboutContainer>
